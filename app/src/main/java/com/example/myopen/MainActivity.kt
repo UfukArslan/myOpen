fd6a4c6741a6ffe6bfae8d55788018449848eb91
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-10-13 22:30:02 +0200

    HelloWorld

---
+++ app/src/main/java/com/example/myopen/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myopen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mGreetingText: TextView
+    private lateinit var mNameInput: EditText
+    private lateinit var mPlayButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mGreetingText=  findViewById<TextView>(R.id.activity_main_greeting_txt)
+        mNameInput= findViewById<EditText>(R.id.activity_main_name_input)
+        mPlayButton =  findViewById<Button>(R.id.activity_main_play_btn)
+    }
+}
+
